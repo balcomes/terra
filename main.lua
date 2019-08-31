7397ce492106f1905688b170d57279856516901c
Shawn Balcome <balcomes@gmail.com>
2019-08-31 03:03:30 -0500

    Fixed Movement

--- main.lua
+++ main.lua
@@ -85,11 +85,7 @@ function love.load()
         for y = 1, gridYCount do
             for x = 1, gridXCount do
                 local cellDrawSize = cellSize - 1
-                --if self.grid[y][x] then
-                --    love.graphics.setColor(self.c1, self.c2, self.c3)
-                --else
-                    love.graphics.setColor(35/255, 40/255, 45/255)
-                --end
+                love.graphics.setColor(35/255, 40/255, 45/255)
                 love.graphics.rectangle(
                     'fill',
                     (x - 1) * cellSize,
@@ -119,18 +115,12 @@ function love.load()
             return this
         end
 
-
         -- Draw Stone
         function Stone:Animate()
             love.graphics.setColor(self.c1, self.c2, self.c3)
             drawCell(self.x, self.y)
         end
 
-        -- Move Stone
-        function Stone:Move()
-            love.graphics.setColor(self.c1, self.c2, self.c3)
-            drawCell(self.x, self.y)
-        end
 --------------------------------------------------------------------------------
 
     board = Board:Create()
@@ -147,94 +137,50 @@ function love.update(dt)
             -- Handle Game Speed
             timer = timer - timerLimit
 
-
-                    -- Player Movement
-            if #player.directionQueue > 1 then
-                table.remove(player.directionQueue, 1)
+            -- Player Movement
+            if love.keyboard.isDown( "up" ) then
+                player.y = player.y - 1
             end
-
-            local nextXPosition = player.x
-            local nextYPosition = player.y
-
-            if player.directionQueue[1] == 'right' then
-                nextXPosition = nextXPosition + 1
-                if nextXPosition > gridXCount then
-                    nextXPosition = 1
-                end
-            elseif player.directionQueue[1] == 'left' then
-                nextXPosition = nextXPosition - 1
-                if nextXPosition < 1 then
-                    nextXPosition = gridXCount
-                end
-            elseif player.directionQueue[1] == 'down' then
-                nextYPosition = nextYPosition + 1
-                if nextYPosition > gridYCount then
-                    nextYPosition = 1
-                end
-            elseif player.directionQueue[1] == 'up' then
-                nextYPosition = nextYPosition - 1
-                if nextYPosition < 1 then
-                    nextYPosition = gridYCount
-                end
+            if love.keyboard.isDown( "down" ) then
+                player.y = player.y + 1
+            end
+            if love.keyboard.isDown( "left" ) then
+                player.x = player.x - 1
+            end
+            if love.keyboard.isDown( "right" ) then
+                player.x = player.x + 1
             end
 
             -- Push Stone
-            if stone.x == nextXPosition
-            and stone.y == nextYPosition then
-                if stone.x == player.x
-                and stone.y == player.y - 1 then
+            if stone.x == player.x
+            and stone.y == player.y then
+                if love.keyboard.isDown( "up" ) then
                     stone.y = stone.y - 1
                 end
-                if stone.x == player.x
-                and stone.y == player.y + 1 then
+                if love.keyboard.isDown( "down" ) then
                     stone.y = stone.y + 1
                 end
-                if stone.y == player.y
-                and stone.x == player.x - 1 then
+                if love.keyboard.isDown( "left" ) then
                     stone.x = stone.x - 1
                 end
-                if stone.y == player.y
-                and stone.x == player.x + 1 then
+                if love.keyboard.isDown( "right" ) then
                     stone.x = stone.x + 1
                 end
             end
-
-            if canMove then
-                player.x = nextXPosition
-                player.y = nextYPosition
-            end
-
-            canMove = false
         end
-
     elseif timer >= 2 then
-
     end
 end
 
 function love.draw()
-
     board:Animate()
     player:Animate()
     stone:Animate()
-
 end
 
--- Player Controls
+-- Menu Controls
 function love.keypressed(key)
-    if key == 'right' then
-        table.insert(player.directionQueue, 'right')
-        canMove = true
-    elseif key == 'left' then
-        table.insert(player.directionQueue, 'left')
-        canMove = true
-    elseif key == 'up' then
-        table.insert(player.directionQueue, 'up')
-        canMove = true
-    elseif key == 'down' then
-        table.insert(player.directionQueue, 'down')
-        canMove = true
-    elseif key == 'q' then
+    if key == 'q' then
         timerLimit = timerLimit + 0.005
     elseif key == 'w' then
         timerLimit = timerLimit - 0.005
